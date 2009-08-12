979616252ac1b2ca142b3e0ff697e766f3b265e8
Brian Cassidy <bricas@cpan.org>
2009-08-12 09:20:59 -0300

    initial pass at moving to libcwiid

--- Wiimote.xs
+++ Wiimote.xs
@@ -4,257 +4,65 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "wiimote.h"
-#include "wiimote_api.h"
+#include <cwiid.h>
+#include "ppport.h"
 
-wiimote_t wiimote = WIIMOTE_INIT;
+typedef cwiid_wiimote_t *Linux__Input__Wiimote;
 
 MODULE = Linux::Input::Wiimote         PACKAGE = Linux::Input::Wiimote
 
 PROTOTYPES: DISABLE
 
-char * c_wiimote_discover()
-     CODE:
-          wiimote_t wiimote[4];
-          int nmotes = 0;
-          int i = 0;
-          nmotes = wiimote_discover(wiimote, 4);
-          RETVAL = " ";
-          if ( nmotes > 0 ) {
-             for (i=0; i<nmotes; i++) {
-                RETVAL =  wiimote[i].link.r_addr;
-             }
-          } else {
-             RETVAL = "0";
-          }
+Linux::Input::Wiimote
+_new( packname="Linux::Input::Wiimote", addr )
+    char *packname
+    char *addr
+CODE:
+    bdaddr_t bdaddr;
+ 
+    if( strlen( addr ) > 0 ) {
+        str2ba( addr, &bdaddr );
+    }
+    else {
+        bdaddr = *BDADDR_ANY;
+    }
 
-     OUTPUT:
-          RETVAL
+    cwiid_wiimote_t *wiimote = NULL;
+    wiimote = cwiid_open( &bdaddr, 0 );
+    RETVAL = wiimote;
+OUTPUT:
+    RETVAL
 
+int
+id( self )
+    Linux::Input::Wiimote self
+CODE:
+    RETVAL = cwiid_get_id( self );
+OUTPUT:
+    RETVAL
 
-int c_wiimote_connect(const char *arg)
-     CODE:
-          RETVAL = wiimote_connect(&wiimote, arg );
-     OUTPUT:
-          RETVAL
+int
+set_led_state( self, state )
+    Linux::Input::Wiimote self
+    unsigned char state
+CODE:
+    cwiid_set_led( self, state );
+OUTPUT:
+    RETVAL
 
-int c_wiimote_is_open()
-     CODE:
-          RETVAL = wiimote_is_open(&wiimote);
-     OUTPUT:
-          RETVAL
+int
+set_rumble( self, rumble )
+    Linux::Input::Wiimote self
+    unsigned char rumble
+CODE:
+    cwiid_set_rumble( self, rumble );
+OUTPUT:
+    RETVAL    
 
-int c_wiimote_update()
-     CODE:
-          RETVAL = wiimote_update(&wiimote);
-     OUTPUT:
-          RETVAL
-
-int c_wiimote_disconnect()
-     CODE:
-          RETVAL =  wiimote_disconnect(&wiimote);
-     OUTPUT:
-          RETVAL
-
-int c_get_wiimote_keys_raw_bits()
-     CODE:
-          RETVAL =  wiimote.keys.bits;
-     OUTPUT:
-          RETVAL
-    
-int c_get_wiimote_rumble()
-     CODE:
-          RETVAL =  wiimote.rumble;
-     OUTPUT:
-          RETVAL
-
-void c_set_wiimote_rumble(int arg)
-     CODE:
-          wiimote.rumble = arg;
-          
-int c_get_wiimote_ir()
-     CODE:
-          RETVAL =  wiimote.mode.ir;
-     OUTPUT:
-          RETVAL
-
-void c_set_wiimote_ir(int arg)
-     CODE:
-          wiimote.mode.ir = arg;
-          
-int c_get_wiimote_ext_nunchuk_joyx()
-     CODE:
-          RETVAL =  wiimote.ext.nunchuk.joyx;
-     OUTPUT:
-          RETVAL
-
-int c_get_wiimote_ext_nunchuk_joyy()
-     CODE:
-          RETVAL =  wiimote.ext.nunchuk.joyy;
-     OUTPUT:
-          RETVAL
-
-int c_get_wiimote_ext_nunchuk_keys_c()
-     CODE:
-          RETVAL =  wiimote.ext.nunchuk.keys.c;
-     OUTPUT:
-          RETVAL
-
-int c_get_wiimote_ext_nunchuk_keys_z()
-     CODE:
-          RETVAL =  wiimote.ext.nunchuk.keys.z;
-     OUTPUT:
-          RETVAL
-
-int c_get_wiimote_ext_nunchuk_axis_x()
-     CODE:
-          RETVAL =  wiimote.ext.nunchuk.axis.x;
-     OUTPUT:
-          RETVAL
-
-int c_get_wiimote_ext_nunchuk_axis_y()
-     CODE:
-          RETVAL =  wiimote.ext.nunchuk.axis.y;
-     OUTPUT:
-          RETVAL
-
-int c_get_wiimote_ext_nunchuk_axis_z()
-     CODE:
-          RETVAL =  wiimote.ext.nunchuk.axis.z;
-     OUTPUT:
-          RETVAL
-
-int c_get_wiimote_axis_x()
-     CODE:
-          RETVAL =  wiimote.axis.x;
-     OUTPUT:
-          RETVAL
-
-int c_get_wiimote_axis_y()
-     CODE:
-          RETVAL =  wiimote.axis.y;
-     OUTPUT:
-          RETVAL
-
-int c_get_wiimote_axis_z()
-     CODE:
-          RETVAL =  wiimote.axis.z;
-     OUTPUT:
-          RETVAL
-
-float c_get_wiimote_tilt_x()
-     CODE:
-          RETVAL =  wiimote.tilt.x;
-     OUTPUT:
-          RETVAL
-
-
-float c_get_wiimote_tilt_y()
-     CODE:
-          RETVAL =  wiimote.tilt.y;
-     OUTPUT:
-          RETVAL
-
-float c_get_wiimote_tilt_z()
-     CODE:
-          RETVAL =  wiimote.tilt.z;
-     OUTPUT:
-          RETVAL
-
-void c_activate_wiimote_accelerometer()
-     CODE:
-           wiimote.mode.acc = 1;
-
-void c_deactivate_wiimote_accelerometer()
-     CODE:
-           wiimote.mode.acc = 0;
-
-int c_get_wiimote_ir1_x()
-     CODE:
-          RETVAL =  wiimote.ir1.x;
-     OUTPUT:
-          RETVAL
-
-int c_get_wiimote_ir1_y()
-     CODE:
-          RETVAL =  wiimote.ir1.y;
-     OUTPUT:
-          RETVAL
-
-int c_get_wiimote_ir1_size()
-     CODE:
-          RETVAL =  wiimote.ir1.size;
-     OUTPUT:
-          RETVAL
-
-int c_get_wiimote_ir2_x()
-     CODE:
-          RETVAL =  wiimote.ir2.x;
-     OUTPUT:
-          RETVAL
-
-int c_get_wiimote_ir2_y()
-     CODE:
-          RETVAL =  wiimote.ir2.y;
-     OUTPUT:
-          RETVAL
-
-int c_get_wiimote_ir2_size()
-     CODE:
-          RETVAL =  wiimote.ir2.size;
-     OUTPUT:
-          RETVAL
-
-int c_get_wiimote_ir3_x()
-     CODE:
-          RETVAL =  wiimote.ir3.x;
-     OUTPUT:
-          RETVAL
-
-int c_get_wiimote_ir3_y()
-     CODE:
-          RETVAL =  wiimote.ir3.y;
-     OUTPUT:
-          RETVAL
-
-int c_get_wiimote_ir3_size()
-     CODE:
-          RETVAL =  wiimote.ir3.size;
-     OUTPUT:
-          RETVAL
-
-int c_get_wiimote_ir4_x()
-     CODE:
-          RETVAL =  wiimote.ir4.x;
-     OUTPUT:
-          RETVAL
-
-int c_get_wiimote_ir4_y()
-     CODE:
-          RETVAL =  wiimote.ir4.y;
-     OUTPUT:
-          RETVAL
-
-int c_get_wiimote_ir4_size()
-     CODE:
-          RETVAL =  wiimote.ir4.size;
-     OUTPUT:
-          RETVAL
-
-float c_get_wiimote_force_x()
-     CODE:
-          RETVAL =  wiimote.force.x;
-     OUTPUT:
-          RETVAL
-
-float c_get_wiimote_force_y()
-     CODE:
-          RETVAL =  wiimote.force.y;
-     OUTPUT:
-          RETVAL
-
-float c_get_wiimote_force_z()
-     CODE:
-          RETVAL =  wiimote.force.z;
-     OUTPUT:
-          RETVAL
+int
+disconnect( self )
+    Linux::Input::Wiimote self
+CODE:
+    RETVAL = cwiid_close( self );
+OUTPUT:
+    RETVAL
--- lib/Linux/Input/Wiimote.pm
+++ lib/Linux/Input/Wiimote.pm
@@ -3,363 +3,101 @@ package Linux::Input::Wiimote;
 use strict;
 use warnings;
 
-use base qw(DynaLoader);
+use base qw( DynaLoader );
 
-our $VERSION = '0.04002';
+our $VERSION = '0.05001';
 
-bootstrap Linux::Input::Wiimote;
-
-use constant {
-    WIIMOTE_KEYS_OFFSET_HOME      => 0,
-    WIIMOTE_KEYS_OFFSET_RESERVED1 => 1,
-    WIIMOTE_KEYS_OFFSET_RESERVED2 => 2,
-    WIIMOTE_KEYS_OFFSET_MINUS     => 3,
-    WIIMOTE_KEYS_OFFSET_A         => 4,
-    WIIMOTE_KEYS_OFFSET_B         => 5,
-    WIIMOTE_KEYS_OFFSET_1         => 6,
-    WIIMOTE_KEYS_OFFSET_2         => 7,
-    WIIMOTE_KEYS_OFFSET_RESERVED3 => 8,
-    WIIMOTE_KEYS_OFFSET_RESERVED4 => 9,
-    WIIMOTE_KEYS_OFFSET_RESERVED5 => 10,
-    WIIMOTE_KEYS_OFFSET_PLUS      => 11,
-    WIIMOTE_KEYS_OFFSET_UP        => 12,
-    WIIMOTE_KEYS_OFFSET_DOWN      => 13,
-    WIIMOTE_KEYS_OFFSET_RIGHT     => 14,
-    WIIMOTE_KEYS_OFFSET_LEFT      => 15,
-};
-
-sub wiimote_discover {
-    my $self = shift;
-    return c_wiimote_discover();
-}
-
-sub wiimote_connect {
-    my $self = shift;
-    my $id   = shift;
-    c_wiimote_connect( $id );
-}
-
-sub wiimote_is_open {
-    my $self = shift;
-    return c_wiimote_is_open();
-}
-
-sub wiimote_update {
-    my $self = shift;
-    c_wiimote_update();
-}
-
-sub wiimote_disconnect {
-    my $self = shift;
-    c_wiimote_disconnect();
-}
-
-sub get_wiimote_rumble {
-    my $self = shift;
-    return c_get_wiimote_rumble();
-}
-
-sub set_wiimote_rumble {
-    my $self = shift;
-    my $setr = shift;
-    c_set_wiimote_rumble( $setr );
-}
-
-sub get_wiimote_ir {
-    my $self = shift;
-    return c_get_wiimote_ir();
-}
-
-sub set_wiimote_ir {
-    my $self = shift;
-    my $setr = shift;
-    c_set_wiimote_ir( $setr );
-}
-
-sub get_wiimote_ext_nunchuk_joyx {
-    my $self = shift;
-    return c_get_wiimote_ext_nunchuk_joyx();
-}
-
-sub get_wiimote_ext_nunchuk_joyy {
-    my $self = shift;
-    return c_get_wiimote_ext_nunchuk_joyy();
-}
-
-sub get_wiimote_ext_nunchuk_keys_c {
-    my $self = shift;
-    return c_get_wiimote_ext_nunchuk_keys_c();
-}
-
-sub get_wiimote_ext_nunchuk_keys_z {
-    my $self = shift;
-    return c_get_wiimote_ext_nunchuk_keys_z();
-}
-
-sub get_wiimote_ext_nunchuk_axis_x {
-    my $self = shift;
-    return c_get_wiimote_ext_nunchuk_axis_x();
-}
-
-sub get_wiimote_ext_nunchuk_axis_y {
-    my $self = shift;
-    return c_get_wiimote_ext_nunchuk_axis_y();
-}
-
-sub get_wiimote_ext_nunchuk_axis_z {
-    my $self = shift;
-    return c_get_wiimote_ext_nunchuk_axis_z();
-}
-
-sub get_wiimote_axis_x {
-    my $self = shift;
-    return c_get_wiimote_axis_x();
-}
-
-sub get_wiimote_axis_y {
-    my $self = shift;
-    return c_get_wiimote_axis_y();
-}
-
-sub get_wiimote_axis_z {
-    my $self = shift;
-    return c_get_wiimote_axis_z();
-}
-
-sub get_wiimote_tilt_x {
-    my $self = shift;
-    return c_get_wiimote_tilt_x();
-}
-
-sub get_wiimote_tilt_y {
-    my $self = shift;
-    return c_get_wiimote_tilt_y();
-}
-
-sub get_wiimote_tilt_z {
-    my $self = shift;
-    return c_get_wiimote_tilt_z();
-}
-
-sub get_wiimote_force_x {
-    my $self = shift;
-    return c_get_wiimote_force_x();
-}
-
-sub get_wiimote_force_y {
-    my $self = shift;
-    return c_get_wiimote_force_y();
-}
-
-sub get_wiimote_force_z {
-    my $self = shift;
-    return c_get_wiimote_force_z();
-}
-
-sub activate_wiimote_accelerometer {
-    my $self = shift;
-    c_activate_wiimote_accelerometer();
-}
-
-sub deactivate_wiimote_accelerometer {
-    my $self = shift;
-    c_deactivate_wiimote_accelerometer();
-}
-
-sub get_wiimote_keys_home {
-    my $self = shift;
-    return _get_wiimote_offset_bit( WIIMOTE_KEYS_OFFSET_HOME );
-}
-
-sub get_wiimote_keys_minus {
-    my $self = shift;
-    return _get_wiimote_offset_bit( WIIMOTE_KEYS_OFFSET_MINUS );
-}
-
-sub get_wiimote_keys_a {
-    my $self = shift;
-    return _get_wiimote_offset_bit( WIIMOTE_KEYS_OFFSET_A );
-}
-
-sub get_wiimote_keys_b {
-    my $self = shift;
-    return _get_wiimote_offset_bit( WIIMOTE_KEYS_OFFSET_B );
-}
-
-sub get_wiimote_keys_1 {
-    my $self = shift;
-    return _get_wiimote_offset_bit( WIIMOTE_KEYS_OFFSET_1 );
-}
-
-sub get_wiimote_keys_2 {
-    my $self = shift;
-    return _get_wiimote_offset_bit( WIIMOTE_KEYS_OFFSET_2 );
-}
-
-sub get_wiimote_keys_plus {
-    my $self = shift;
-    return _get_wiimote_offset_bit( WIIMOTE_KEYS_OFFSET_PLUS );
-}
-
-sub get_wiimote_keys_up {
-    my $self = shift;
-    return _get_wiimote_offset_bit( WIIMOTE_KEYS_OFFSET_UP );
-}
-
-sub get_wiimote_keys_down {
-    my $self = shift;
-    return _get_wiimote_offset_bit( WIIMOTE_KEYS_OFFSET_DOWN );
-}
-
-sub get_wiimote_keys_right {
-    my $self = shift;
-    return _get_wiimote_offset_bit( WIIMOTE_KEYS_OFFSET_RIGHT );
-}
-
-sub get_wiimote_keys_left {
-    my $self = shift;
-    return _get_wiimote_offset_bit( WIIMOTE_KEYS_OFFSET_LEFT );
-}
-
-sub get_wiimote_keys_bits {
-    my $self = shift;
-    return dec2bin16( c_get_wiimote_keys_raw_bits() );
-}
-
-sub _get_wiimote_offset_bit {
-    my $offset = shift;
-    return substr( dec2bin16( c_get_wiimote_keys_raw_bits() ), $offset, 1 );
-}
-
-sub dec2bin16 {
-    my $str = unpack( "B32", pack( "N", shift ) );
-    return substr( $str, 16, 16 );
-}
+bootstrap Linux::Input::Wiimote $VERSION;
 
 sub new {
-    my $self = {};
-    bless( $self );    # but see below
-    return $self;
+    shift->_new( shift || '' );
 }
+
+1;
+
+__END__
 
 =head1 NAME
 
-Linux::Input::Wiimote - Perl interface to the libcwiimote library
+Linux::Input::Wiimote - Perl interface to the libcwiid library
 
 =head1 SYNOPSIS
 
-    This is a perl interface to the C library libcwiimote ( http://sourceforge.net/projects/libwiimote/ ).
-    It implements most of that API and provides access to most functions of the wiimote.
+    # search for a wiimote
+    # NB: must be in discovery mode (press buttons 1 + 2)
+    my $wiimote = Linux::Input::Wiimote->new;
 
-    libcwiimote version 0.04 must be installed and the bluetooth dameon must be running
+    # turn on rumble for 3 seconds
+    $wiimote->set_rumble( 1 );
+    sleep( 3 );
+    $wiimote->set_rumble( 0 );
 
-    Auto discovery is not yet built in.  You must know the id of your wiimote.  You can use hcitool scan
-    to find the id.
+=head1 DESCRIPTION
+
+This module aims to provide simple perl bindings for the libcwiid library.
 
 =head1 METHODS
 
-    wiimote_connect( ID OF YOUR WIIMOTE ): pass in the id of your wiimote such as '00:19:1D:75:CC:30'.  Returns 0 on success
-    wiimote_update : updates the wiimote.  Should be called often
+=head2 new( [$addr] )
 
-    wiimote_discover: discovers and returns the id of the last wiimote it sees.  WARNING, this method has bugs and will segfault if it doesn't find a wii quickly
-    set_wiimote_rumble:  starts/stops rumble.  Pass in 1 to start, 0 to stop
-    set_wiimote_ir:  starts/stops ir sensor.  Pass in 1 to start, 0 to stop
+Connects to a wiimote. You may either specify a specific address, or leave
+it out entirely to let the library pick the first available wiimote. Returns 
+undef on failure.
 
-    get_wiimote_tilt_x
-    get_wiimote_tilt_y
-    get_wiimote_tilt_z
+=head2 id( )
 
-    get_wiimote_axis_x
-    get_wiimote_axis_y
-    get_wiimote_axis_z
+Returns a unique integer represeting this wiimote.
 
-    get_wiimote_ext_nunchuk_axis_x
-    get_wiimote_ext_nunchuk_axis_y
-    get_wiimote_ext_nunchuk_axis_z
+=head2 set_led_state( $flag )
 
-    get_wiimote_ext_nunchuk_joyx
-    get_wiimote_ext_nunchuk_joyy
+Set the state of the LEDs on the wiimote. C<$flag> is a simple bitmask
+where each bit represents an LED.
 
-    IR sensor position (must first set_wiimote_ir(1) to start ir sensor) NOT YET FINISHED:
-    get_wiimote_ir1_y
-    get_wiimote_ir1_x
-    get_wiimote_ir1_size
+    # turn the 3rd LED on
+    $wiimote->set_led_state( 4 );
 
-    get_wiimote_ir2_y
-    get_wiimote_ir2_x
-    get_wiimote_ir2_size
+    # turn the 1st and 4th LEDs on
+    $wiimote->set_led_state( 9 );
 
-    get_wiimote_ir3_y
-    get_wiimote_ir3_x
-    get_wiimote_ir3_size
+=head2 set_rumble( $flag )
 
-    get_wiimote_ir4_y
-    get_wiimote_ir4_x
-    get_wiimote_ir4_size
+Set the state of the rumble function on the wiimote. C<$flag> is a simple
+boolean value.
 
-    Methods below return 0 or 1 depending on if key is pressed:
+    # turn rumble on
+    $wiimote->set_rumble( 1 );
 
-    get_wiimote_keys_home
-    get_wiimote_keys_minus
-    get_wiimote_keys_a
-    get_wiimote_keys_b
-    get_wiimote_keys_1
-    get_wiimote_keys_2
-    get_wiimote_keys_plus
-    get_wiimote_keys_up
-    get_wiimote_keys_down
-    get_wiimote_keys_right
-    get_wiimote_keys_left
-    get_wiimote_keys_bits
+    # turn rumble off
+    $wiimote->set_rumble( 0 );
 
-=head1 EXAMPLE
+=head2 disconnect( )
 
-    use Linux::Input::Wiimote;
+Disconnect the wiimote.
 
-    my $wii = new Linux::Input::Wiimote;
-
-    $wii->wiimote_connect('00:19:1D:75:CC:30');
-
-    while ( $wii->is_open() ) {
-        $wii->wiimote_update();
-        print "Wiimote Key bits: " . $wii->get_wiimote_keys_bits() . "\n";
-    }
-
-=head1 TODO
+=head1 SEE ALSO
 
 =over 4
 
-=item * Add support for multiple remotes (it is already in libcmote)
-
-=item * Add auto descovery of wiimote 
-
-=back
-
-=head1 KNOWN BUGS
-
-=over 4
-
-=item * wiimote_discover can cause a segfault
+=item * http://abstrakraft.org/cwiid/wiki/libcwiid
 
 =back
 
 =head1 AUTHOR
 
+Brian Cassidy E<lt>bricas@cpan.orgE<gt>
+
+=head1 ORIGINALLY WRITTEN BY
+
 Chad Phillips E<lt>chad@chadphillips.orgE<gt>
-
-=head1 MAINTAINER
-
-Brian Cassidy E<lt>bricas@cpan.org<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
 Copyright 2007 by Chad Phillips
 
-Copyright 2007 by Brian Cassidy
+Copyright 2007-2009 by Brian Cassidy
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself. 
 
 =cut
 
-1;
