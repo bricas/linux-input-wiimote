9ff4fbe5c060c217212599a989aec7c8c082c7d6
Brian Cassidy <bricas@cpan.org>
2009-08-26 20:28:00 -0300

    add convenience methods for wiimote buttons

--- lib/Linux/Input/Wiimote/State.pm
+++ lib/Linux/Input/Wiimote/State.pm
@@ -35,4 +35,48 @@ sub acc_z {
     return shift->acc->[ 2 ];
 }
 
+sub btn_2 {
+    return shift->buttons & 0x0001 ? 1 : 0;
+}
+
+sub btn_1 {
+    return shift->buttons & 0x0002 ? 1 : 0;
+}
+
+sub btn_b {
+    return shift->buttons & 0x0004 ? 1 : 0;
+}
+
+sub btn_a {
+    return shift->buttons & 0x0008 ? 1 : 0;
+}
+
+sub btn_minus {
+    return shift->buttons & 0x0010 ? 1 : 0;
+}
+
+sub btn_home {
+    return shift->buttons & 0x0080 ? 1 : 0;
+}
+
+sub btn_left {
+    return shift->buttons & 0x0100 ? 1 : 0;
+}
+
+sub btn_right {
+    return shift->buttons & 0x0200 ? 1 : 0;
+}
+
+sub btn_down {
+    return shift->buttons & 0x0400 ? 1 : 0;
+}
+
+sub btn_up {
+    return shift->buttons & 0x0800 ? 1 : 0;
+}
+
+sub btn_plus {
+    return shift->buttons & 0x1000 ? 1 : 0;
+}
+
 1;
