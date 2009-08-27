2ed0a949f1a8cfbd133fca4dfd1808b8c2d48d9e
Brian Cassidy <bricas@cpan.org>
2009-08-26 21:41:58 -0300

    generate methods

--- lib/Linux/Input/Wiimote/State.pm
+++ lib/Linux/Input/Wiimote/State.pm
@@ -5,78 +5,32 @@ use namespace::autoclean;
 
 has [ qw(rumble battery report_mode led buttons acc ext_type ) ] => ( is => 'ro', init_arg => undef );
 
+for my $l ( 0..3 ) {
+    __PACKAGE__->meta->add_method( 'led_' . ( $l + 1 ) => sub { shift->led & ( 2 ** $l ) ? 1 : 0 } );
+}
+
+my @acc = qw( x y z );
+for my $ac ( 0..2 ) {
+    __PACKAGE__->meta->add_method( 'acc' . ( $acc[ $ac ] ) => sub { shift->acc->[ $ac ] } );
+} 
+
+my %btn = (
+    2     => 0x0001,
+    1     => 0x0002,
+    b     => 0x0004,
+    a     => 0x0008,
+    minus => 0x0010,
+    home  => 0x0080,
+    left  => 0x0100,
+    right => 0x0200,
+    down  => 0x0400,
+    up    => 0x0800,
+    plus  => 0x1000,
+);
+for my $bt ( keys %btn ) {
+    __PACKAGE__->meta->add_method( "button_${bt}" => sub { shift->buttons & $btn{ $bt } ? 1 : 0 } );
+}
+
 __PACKAGE__->meta->make_immutable;
-
-sub led_1 {
-    return shift->led & 0x01 ? 1 : 0;
-}
-
-sub led_2 {
-    return shift->led & 0x02 ? 1 : 0;
-}
-
-sub led_3 {
-    return shift->led & 0x04 ? 1 : 0;
-}
-
-sub led_4 {
-    return shift->led & 0x08 ? 1 : 0;
-}
-
-sub acc_x {
-    return shift->acc->[ 0 ];
-}
-
-sub acc_y {
-    return shift->acc->[ 1 ];
-}
-
-sub acc_z {
-    return shift->acc->[ 2 ];
-}
-
-sub btn_2 {
-    return shift->buttons & 0x0001 ? 1 : 0;
-}
-
-sub btn_1 {
-    return shift->buttons & 0x0002 ? 1 : 0;
-}
-
-sub btn_b {
-    return shift->buttons & 0x0004 ? 1 : 0;
-}
-
-sub btn_a {
-    return shift->buttons & 0x0008 ? 1 : 0;
-}
-
-sub btn_minus {
-    return shift->buttons & 0x0010 ? 1 : 0;
-}
-
-sub btn_home {
-    return shift->buttons & 0x0080 ? 1 : 0;
-}
-
-sub btn_left {
-    return shift->buttons & 0x0100 ? 1 : 0;
-}
-
-sub btn_right {
-    return shift->buttons & 0x0200 ? 1 : 0;
-}
-
-sub btn_down {
-    return shift->buttons & 0x0400 ? 1 : 0;
-}
-
-sub btn_up {
-    return shift->buttons & 0x0800 ? 1 : 0;
-}
-
-sub btn_plus {
-    return shift->buttons & 0x1000 ? 1 : 0;
-}
 
 1;
